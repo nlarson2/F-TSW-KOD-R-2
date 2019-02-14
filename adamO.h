317316984dba30f175b1dfee09a525cd9b1affe3
Adam Ostlund <aostlund@odin.cs.csubak.edu>
2019-02-14 12:20:37 -0800

    my first commit

---
+++ adamO.h
@@ -0,0 +1,26 @@
+//3350
+//program: adamO.cpp
+//author: adam ostlund
+//date: february 14, 2019
+//
+//This program is an individual source file containing a name and profile picture.
+//
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+#include <math.h>
+#include <X11/Xlib.h>
+//#include <X11/Xutil.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <X11/keysym.h>
+#include <GL/glx.h>
+#include "log.h"
+//#include "ppm.h"
+#include "fonts.h"
+
+void showAdamPicture(int x, int y, GLuint textid);
