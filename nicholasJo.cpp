6c349b21c473553940923a81d0ceed9549026f16
Nicholas Jordan <njordan@odin.cs.csubak.edu>
2019-02-18 11:07:42 -0800

    removed unnecessary includes

--- nicholasJo.cpp
+++ nicholasJo.cpp
@@ -5,20 +5,7 @@
 //
 // This program displays my name and picture as part of the credits of our game
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <math.h>
-#include <X11/Xlib.h>
-//#include <X11/Xutil.h>
-//#include <GL/gl.h>
-//#include <GL/glu.h>
-#include <X11/keysym.h>
-#include <GL/glx.h>
-#include "log.h"
-//#include "ppm.h"
+#include <GL/gl.h>
 #include "fonts.h"
 
 void Display_NicholasJordan(int x, int y, GLuint textid) {
