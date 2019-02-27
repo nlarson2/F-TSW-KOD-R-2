6c5ae9d1d567aaf001050a3a12a78df215b8bbab
njordan3 <njordan3@csub.edu>
2019-02-27 10:28:12 -0800

    the beginnings of player and NPC functions

--- nicholasJo.cpp
+++ nicholasJo.cpp
@@ -2,11 +2,154 @@
 // program: nicholasJo.cpp
 // author: Nicholas Jordan
 // date: 02/14/2019
-//
-// This program displays my name and picture as part of the credits of our game
 
-#include <GL/gl.h>
-#include "fonts.h"
+#ifndef NICHOLAS_JO_CPP
+#define NICHOLAS_JO_CPP
+
+#include "nicholasJo.h"
+
+//==========================[ENTITY CLASS]===============================
+//Parent Class
+
+Entity::Entity() {
+}
+
+float Entity::getMaxHealth() {
+	return max_health;
+}
+
+float Entity::getCurrentHealth() {
+	if (current_health < 0.0) {
+		//negative health is not possible
+		return 0.0;
+	}
+	if (current_health > max_health) {
+		//ensures no overheal
+		current_health = max_health;
+	}
+	return current_health;
+}
+
+float Entity::getDefense() {
+	return defense;
+}
+
+void Entity::setAlly(bool a) {
+	ally = a;
+}
+
+bool Entity::getAlly() {
+	return ally;
+}
+
+void Entity::setMaxHealth(float h) {
+	max_health = h;
+}
+
+void Entity::setDefense(float d) {
+	defense = d;
+}
+
+//==========================[ALLY CLASS]===============================
+//Inherits from Entity
+
+Ally::Ally() {
+	setAlly(true);
+	setAllyCombatType();
+}
+
+void Ally::setAllyCombatType() {
+	//randomly generate a number from 0 to 2 to choose combat type
+	int random = rand() % 3;
+	switch (random) {
+		case 0 :
+			combatType = "archer";
+			setMaxHealth(50.0);
+			setDefense(0.3);
+			break;
+		case 1:
+			combatType = "soldier";
+			setMaxHealth(75.0);
+			setDefense(0.5);
+			break;
+		case 2:
+			combatType = "tank";
+			setMaxHealth(100);
+			setDefense(0.7);
+			break;
+	}
+	return;
+}
+
+//==========================[ENEMY CLASS]===============================
+//Inherits from Entity
+
+Enemy::Enemy() {
+	setAlly(false);
+	setEnemyCombatType();
+}
+
+void Enemy::setEnemyCombatType() {
+	//randomly generate a number from 0 to 2 to choose combat type
+	int random = rand() % 3;
+	switch (random) {
+		case 0 :
+			combatType = "archer";
+			setMaxHealth(50.0);
+			setDefense(0.3);
+			break;
+		case 1:
+			combatType = "soldier";
+			setMaxHealth(75.0);
+			setDefense(0.5);
+			break;
+		case 2:
+			combatType = "tank";
+			setMaxHealth(100);
+			setDefense(0.7);
+			break;
+	}
+	return;
+}
+
+//==========================[PLAYER CLASS]===============================
+//Inherits from Entity
+
+Player::Player() {
+	setPlayerCombatType();
+}
+
+//Null because instance will be initialized on demand
+Player* Player::instance = 0;
+
+Player* Player::getInstance() {
+	if (instance == 0) {
+		instance = new Player();
+	}
+	return instance;
+}
+
+Player::~Player() {
+	delete []instance;
+}
+
+void Player::setPlayerCombatType() {
+	//need to make a menu function so the player can choose combat type
+	
+	if (combatType == "archer") {
+		setMaxHealth(75.0);
+		setDefense(0.60);
+	} else if (combatType == "soldier") {
+		setMaxHealth(100.0);
+		setDefense(0.70);
+	} else if (combatType == "tank") {
+		setMaxHealth(125.0);
+		setDefense(0.80);
+	}
+	return;
+}
+
+//============================================================================
 
 void Display_NicholasJordan(int x, int y, GLuint textid) {
     glColor3ub(255, 255, 255);
@@ -40,3 +183,5 @@ void Display_NicholasJordan(int x, int y, GLuint textid) {
     ggprint8b(&r, 16, 0x00000000, "Nicholas Jordan");
     glPopMatrix();
 }
+
+#endif
---
+++ nicholasJo.h
@@ -0,0 +1,62 @@
+//Author: Nicholas Jordan
+//Date: 02/24/2019
+
+#include <GL/glx.h>
+#include "fonts.h"
+
+using namespace std;
+
+#ifndef NICHOLAS_JO_H
+#define NICHOLAS_JO_H
+
+void Display_NicholasJordan(int, int, GLuint);
+
+class Entity
+{
+	private:
+		float max_health;
+		float defense;
+		bool ally;
+	public:
+		Entity();
+		float getMaxHealth();
+		float getCurrentHealth();
+		void setMaxHealth(float);
+		float getDefense();
+		void setDefense(float);
+		void setAlly(bool);
+		bool getAlly();		
+
+		string combatType;
+		float current_health;
+};
+
+class Ally : public Entity
+{
+	public:
+		Ally();
+		void setAllyCombatType();
+};
+
+class Enemy : public Entity
+{
+	public:
+		Enemy();
+		void setEnemyCombatType();
+};
+
+//singleton class
+//initialize player with: Player *player = Player::getInstance();
+class Player : public Entity
+{
+	public:
+		static Player* getInstance();
+		//void PlayerMovement(XEvent *e);
+		void setPlayerCombatType();
+	private:
+		static Player* instance;
+		Player();
+		~Player();
+};
+
+#endif
